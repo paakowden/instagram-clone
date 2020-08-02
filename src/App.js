4b84ba9e6183cb47de3e135b068b0f025ae610fc
paakowden <paakow1114@gmail.com>
2020-08-02 15:30:51 -0400

    Instagram-clone

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />
+      </div>
+      <h1>Hi there</h1>
+      {/* Posts */}
+      {/* Posts */}
+    </div>
+  );
+}
+
+export default App;
